ea03e073ea6c8fb84326106ab71969b95571de8a
pazouki-tera <157961036+pazouki-tera@users.noreply.github.com>
2025-11-10 14:47:39 +0330

    Include RedisAPL in APL imports and initialization
    Added RedisAPL support for APL initialization.

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -1,8 +1,12 @@
-import { APL, FileAPL, SaleorCloudAPL, UpstashAPL } from "@saleor/app-sdk/APL";
+import { APL, FileAPL, SaleorCloudAPL, UpstashAPL, RedisAPL } from "@saleor/app-sdk/APL";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 import { invariant } from "./lib/invariant";
 export let apl: APL;
 switch (process.env.APL) {
+  case "redis": {
+    const redisUrl = process.env.REDIS_URL;
+    apl = new RedisAPL(redisUrl);
+    break;
   case "saleor-cloud":
     const token = process.env.REST_APL_TOKEN;
     const endpoint = process.env.REST_APL_ENDPOINT;
